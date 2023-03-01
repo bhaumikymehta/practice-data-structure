5438a422e6c75ab75e2166f433fd15eb4a331428
bhaumikmehta <bhaumikymehta@gmail.com>
2023-03-01 10:06:43 -0600

    adding more practice questions

---
+++ src/main/java/easy/Anagram.java
@@ -0,0 +1,40 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Anagram {
+    
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        Map<Character,Integer> map = new HashMap<>();
+        for(Character c: s.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)+1);
+            }
+            else{
+                map.put(c,1);
+            }
+        }
+        for(Character c: t.toCharArray()){
+            if(map.containsKey(c)){
+                if(map.get(c)-1<0){
+                    return false;
+                }
+                map.put(c,map.get(c)-1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class SolutionAnagram{
+    public static void main(String[] args) {
+        Anagram anagram = new Anagram();
+        boolean result=anagram.isAnagram("test","estt");
+        System.out.println("Result for valid Anagram :"+result);
+    }
+}
---
+++ src/main/java/easy/InvertBinaryTree.java
@@ -0,0 +1,36 @@
+package easy;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
