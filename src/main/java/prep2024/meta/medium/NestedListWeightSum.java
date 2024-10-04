1e1b7a9de4186938f56b20f9873c1796833a7b19
bhaumikmehta <bhaumikymehta@gmail.com>
2024-10-04 17:36:16 -0500

    Meta medium

---
+++ src/main/java/prep2024/meta/medium/NestedListWeightSum.java
@@ -0,0 +1,62 @@
+package prep2024.meta.medium;
+
+public class NestedListWeightSum {
+
+    /**
+     * This is the interface that allows for creating nested lists.
+     * You should not implement it, or speculate about its implementation
+     */
+    public interface NestedInteger {
+        // Constructor initializes an empty nested list.
+        public NestedInteger();
+
+        // Constructor initializes a single integer.
+        public NestedInteger(int value);
+
+        // @return true if this NestedInteger holds a single integer, rather than a
+        // nested list.
+        public boolean isInteger();
+
+        // @return the single integer that this NestedInteger holds, if it holds a
+        // single integer
+        // Return null if this NestedInteger holds a nested list
+        public Integer getInteger();
+
+        // Set this NestedInteger to hold a single integer.
+        public void setInteger(int value);
+
+        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+        public void add(NestedInteger ni);
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested
+        // list
+        // Return empty list if this NestedInteger holds a single integer
+        public List<NestedInteger> getList();
+    }
+
+    class Solution {
+        public int depthSum(List<NestedInteger> nestedList) {
+            Deque<NestedInteger> queue = new ArrayDeque<>();
+            // Queue<NestedInteger> queue = new LinkedList<>();
+            queue.addAll(nestedList);
+
+            int depth = 1;
+            int total = 0;
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    NestedInteger nested = queue.poll();
+                    if (nested.isInteger()) {
+                        total += nested.getInteger() * depth;
+                    } else {
+                        queue.addAll(nested.getList());
+                    }
+                }
+                depth++;
+            }
+            return total;
+        }
+    }
+
+}
